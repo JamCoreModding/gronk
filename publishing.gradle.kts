0153615241b23f88ea92eaf209282709cbd9c14d
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-05-07 18:48:07 +0100

    chore: attempt dropping githubRelease version

--- publishing.gradle.kts
+++ publishing.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 
     dependencies {
         classpath("gradle.plugin.com.matthewprenger:CurseGradle:1.4.0")
-        classpath("com.github.breadmoirai:github-release:2.3.6")
+        classpath("com.github.breadmoirai:github-release:2.2.12")
     }
 }
 
