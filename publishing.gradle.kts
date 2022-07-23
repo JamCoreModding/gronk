253b6f46e460cd59b404ce1f5d4b1606c5919ab7
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-07-23 17:29:55 +0100

    feat: cleanup, allow beta releases

--- publishing.gradle.kts
+++ publishing.gradle.kts
@@ -4,11 +4,7 @@ import com.matthewprenger.cursegradle.Options
 import java.util.*
 
 buildscript {
-    repositories {
-        maven {
-            url = uri("https://plugins.gradle.org/m2/")
-        }
-    }
+    repositories { maven { url = uri("https://plugins.gradle.org/m2/") } }
 
     dependencies {
         classpath("gradle.plugin.com.matthewprenger:CurseGradle:1.4.0")
@@ -18,8 +14,11 @@ buildscript {
 }
 
 apply<com.matthewprenger.cursegradle.CurseGradlePlugin>()
+
 apply<com.modrinth.minotaur.Minotaur>()
+
 apply<com.github.breadmoirai.githubreleaseplugin.GithubReleasePlugin>()
+
 apply(plugin = "maven-publish")
 
 val secrets = Properties()
@@ -37,11 +36,15 @@ fun getGradleSecret(name: String): String? {
 }
 
 fun isCurseforgeEnabled(): Boolean {
-    return getGradleSecret("curseforge_api_key") != null && getGradleProperty("curseforge_project_id") != null && getGradleProperty("supported_versions") != null
+    return getGradleSecret("curseforge_api_key") != null &&
+            getGradleProperty("curseforge_project_id") != null &&
+            getGradleProperty("supported_versions") != null
 }
 
 fun isModrinthEnabled(): Boolean {
-    return getGradleSecret("modrinth_api_key") != null && getGradleProperty("modrinth_project_id") != null && getGradleProperty("supported_versions") != null
+    return getGradleSecret("modrinth_api_key") != null &&
+            getGradleProperty("modrinth_project_id") != null &&
+            getGradleProperty("supported_versions") != null
 }
 
 fun isGithubEnabled(): Boolean {
@@ -49,124 +52,140 @@ fun isGithubEnabled(): Boolean {
 }
 
 if (isCurseforgeEnabled()) {
-    println("Enabling CurseForge publishing")
+    println("Curseforge Publishing: ☑")
 
     configure<com.matthewprenger.cursegradle.CurseExtension> {
         apiKey = getGradleSecret("curseforge_api_key")!!
 
-        project(closureOf<CurseProject> {
-            id = getGradleProperty("curseforge_project_id")!!
-            changelogType = "markdown"
-            releaseType = "release"
-            changelog = project.rootProject.file("CHANGELOG.md")
-            mainArtifact(tasks.get("remapJar"))
-            mainArtifact.displayName = getGradleProperty("release_name")!!
+        project(
+                closureOf<CurseProject> {
+                    id = getGradleProperty("curseforge_project_id")!!
+                    changelogType = "markdown"
+                    releaseType =
+                            if (getGradleProperty("mod_version").contains("beta")) "beta"
+                            else "release"
+                    changelog = project.rootProject.file("CHANGELOG.md")
+                    mainArtifact(tasks.get("remapJar"))
+                    mainArtifact.displayName = getGradleProperty("release_name")!!
 
-            afterEvaluate {
-                uploadTask.dependsOn("remapJar")
-            }
+                    afterEvaluate { uploadTask.dependsOn("remapJar") }
 
-            addGameVersion("Fabric")
-	    addGameVersion("Quilt")
-	    
-            getGradleProperty("supported_versions")!!.split(",").forEach {
-                addGameVersion(it)
-            }
-	    
-	    if (
-	        getGradleProperty("curseforge_required_dependencies") != null ||
-		getGradleProperty("curseforge_embedded_dependencies") != null ||
-		getGradleProperty("curseforge_optional_dependencies") != null ||
-		getGradleProperty("curseforge_tool_dependencies") != null ||
-		getGradleProperty("curseforge_incompatible_dependencies") != null 
-	    )
-            relations(closureOf<CurseRelation> {
-                if (getGradleProperty("curseforge_required_dependencies") != null) {
-                    getGradleProperty("curseforge_required_dependencies")!!.split(",").forEach {
-                        requiredDependency(it)
+                    addGameVersion("Fabric")
+                    addGameVersion("Quilt")
+
+                    getGradleProperty("supported_versions")!!.split(",").forEach {
+                        addGameVersion(it)
                     }
-                }
 
-                if (getGradleProperty("curseforge_embedded_dependencies") != null) {
-                    getGradleProperty("curseforge_embedded_dependencies")!!.split(",").forEach {
-                        embeddedLibrary(it)
-                    }
-                }
+                    if (getGradleProperty("curseforge_required_dependencies") != null ||
+                                    getGradleProperty("curseforge_embedded_dependencies") != null ||
+                                    getGradleProperty("curseforge_optional_dependencies") != null ||
+                                    getGradleProperty("curseforge_tool_dependencies") != null ||
+                                    getGradleProperty("curseforge_incompatible_dependencies") !=
+                                            null
+                    )
+                            relations(
+                                    closureOf<CurseRelation> {
+                                        if (getGradleProperty("curseforge_required_dependencies") !=
+                                                        null
+                                        ) {
+                                            getGradleProperty("curseforge_required_dependencies")!!
+                                                    .split(",")
+                                                    .forEach { requiredDependency(it) }
+                                        }
 
-                if (getGradleProperty("curseforge_optional_dependencies") != null) {
-                    getGradleProperty("curseforge_optional_dependencies")!!.split(",").forEach {
-                        optionalDependency(it)
-                    }
-                }
+                                        if (getGradleProperty("curseforge_embedded_dependencies") !=
+                                                        null
+                                        ) {
+                                            getGradleProperty("curseforge_embedded_dependencies")!!
+                                                    .split(",")
+                                                    .forEach { embeddedLibrary(it) }
+                                        }
 
-                if (getGradleProperty("curseforge_tool_dependencies") != null) {
-                    getGradleProperty("curseforge_tool_dependencies")!!.split(",").forEach {
-                        tool(it)
-                    }
-                }
+                                        if (getGradleProperty("curseforge_optional_dependencies") !=
+                                                        null
+                                        ) {
+                                            getGradleProperty("curseforge_optional_dependencies")!!
+                                                    .split(",")
+                                                    .forEach { optionalDependency(it) }
+                                        }
 
-                if (getGradleProperty("curseforge_incompatible_dependencies") != null) {
-                    getGradleProperty("curseforge_incompatible_dependencies")!!.split(",").forEach {
-                        incompatible(it)
-                    }
-                }
-            })
-        })
+                                        if (getGradleProperty("curseforge_tool_dependencies") !=
+                                                        null
+                                        ) {
+                                            getGradleProperty("curseforge_tool_dependencies")!!
+                                                    .split(",")
+                                                    .forEach { tool(it) }
+                                        }
 
-        options(closureOf<Options> {
-            forgeGradleIntegration = false
-        })
+                                        if (getGradleProperty(
+                                                        "curseforge_incompatible_dependencies"
+                                                ) != null
+                                        ) {
+                                            getGradleProperty(
+                                                            "curseforge_incompatible_dependencies"
+                                                    )!!
+                                                    .split(",")
+                                                    .forEach { incompatible(it) }
+                                        }
+                                    }
+                            )
+                }
+        )
+
+        options(closureOf<Options> { forgeGradleIntegration = false })
     }
 } else {
-    println("Not enabling CurseForge publishing")
+    println("Curseforge Publishing: ☒")
 }
 
 if (isModrinthEnabled()) {
-    println("Enabling Modrinth publishing")
+    println("Modrinth Publishing: ☑")
 
     configure<com.modrinth.minotaur.ModrinthExtension> {
-	versionNumber.set(getGradleProperty("mod_version")!!)
-	versionName.set(getGradleProperty("release_name")!!)
+        versionNumber.set(getGradleProperty("mod_version")!!)
+        versionName.set(getGradleProperty("release_name")!!)
+        versionType.set(
+                if (getGradleProperty("mod_version").contains("beta")) "beta" else "release"
+        )
         token.set(getGradleSecret("modrinth_api_key"))
         projectId.set(getGradleProperty("modrinth_project_id")!!)
         uploadFile.set(tasks.get("remapJar"))
         gameVersions.addAll(getGradleProperty("supported_versions")!!.split(","))
         loaders.addAll(listOf("fabric", "quilt"))
         changelog.set(project.rootProject.file("CHANGELOG.md").readText())
-	
+
         syncBodyFrom.set(project.rootProject.file("README.md").readText())
 
-        if (
-	        getGradleProperty("modrinth_required_dependencies") != null ||
-		getGradleProperty("modrinth_optional_dependencies") != null ||
-		getGradleProperty("modrinth_incompatible_dependencies") != null 
-	    )
-	    configure<com.modrinth.minotaur.dependencies.container.DependencyDSL> {
-                if (getGradleProperty("modrinth_required_dependencies") != null) {
-                    getGradleProperty("modrinth_required_dependencies")!!.split(",").forEach {
-                        required.project(it)
+        if (getGradleProperty("modrinth_required_dependencies") != null ||
+                        getGradleProperty("modrinth_optional_dependencies") != null ||
+                        getGradleProperty("modrinth_incompatible_dependencies") != null
+        )
+                configure<com.modrinth.minotaur.dependencies.container.DependencyDSL> {
+                    if (getGradleProperty("modrinth_required_dependencies") != null) {
+                        getGradleProperty("modrinth_required_dependencies")!!.split(",").forEach {
+                            required.project(it)
+                        }
                     }
-                }
 
-                if (getGradleProperty("modrinth_optional_dependencies") != null) {
-                    getGradleProperty("modrinth_optional_dependencies")!!.split(",").forEach {
-                        optional.project(it)
+                    if (getGradleProperty("modrinth_optional_dependencies") != null) {
+                        getGradleProperty("modrinth_optional_dependencies")!!.split(",").forEach {
+                            optional.project(it)
+                        }
                     }
-                }
 
-                if (getGradleProperty("modrinth_incompatible_dependencies") != null) {
-                    getGradleProperty("modrinth_incompatible_dependencies")!!.split(",").forEach {
-                        incompatible.project(it)
+                    if (getGradleProperty("modrinth_incompatible_dependencies") != null) {
+                        getGradleProperty("modrinth_incompatible_dependencies")!!.split(",")
+                                .forEach { incompatible.project(it) }
                     }
                 }
-            }
     }
 } else {
-    println("Not enabling Modrinth publishing")
+    println("Modrinth Publishing: ☒")
 }
 
 if (isGithubEnabled()) {
-    println("Enabling GitHub publishing")
+    println("GitHub Publishing: ☑")
 
     configure<com.github.breadmoirai.githubreleaseplugin.GithubReleaseExtension> {
         token(getGradleSecret("github_token"))
@@ -175,8 +194,8 @@ if (isGithubEnabled()) {
         tagName(getGradleProperty("mod_version"))
         releaseName(getGradleProperty("release_name"))
         body(project.rootProject.file("CHANGELOG.md").readText())
-        prerelease(false)
-	draft(true)	
+        prerelease(getGradleProperty("mod_version").contains("beta"))
+        draft(false)
 
         if (getGradleProperty("release_branch") != null) {
             targetCommitish(getGradleProperty("release_branch"))
@@ -187,21 +206,31 @@ if (isGithubEnabled()) {
 
         if (libsDir.exists() && devLibsDir.exists()) {
             val archiveBaseName = getGradleProperty("archive_base_name")!!
-            val libs = libsDir.listFiles().filter { it.name.endsWith(".jar") && it.name.contains(archiveBaseName) }
-            val devLibs = devLibsDir.listFiles().filter { it.name.endsWith(".jar") && it.name.contains(archiveBaseName) }
+            val libs =
+                    libsDir.listFiles().filter {
+                        it.name.endsWith(".jar") && it.name.contains(archiveBaseName)
+                    }
+            val devLibs =
+                    devLibsDir.listFiles().filter {
+                        it.name.endsWith(".jar") && it.name.contains(archiveBaseName)
+                    }
             releaseAssets(libs, devLibs)
         }
     }
 } else {
-    println("Not enabling GitHub publishing")
+    println("GitHub Publishing: ☒")
 }
 
 tasks {
     named("publish") {
         dependsOn("jar")
         dependsOn("remapJar")
-	dependsOn("build")
-        
+        dependsOn("build")
+
+        if (isGithubEnabled()) {
+            dependsOn("githubRelease")
+        }
+
         if (isCurseforgeEnabled()) {
             dependsOn("curseforge")
         }
@@ -210,11 +239,7 @@ tasks {
             dependsOn("modrinth")
         }
 
-        if (isGithubEnabled()) {
-            dependsOn("githubRelease")
-        }
-	
-	dependsOn("modrinthSyncBody")
+        dependsOn("modrinthSyncBody")
 
         doLast {
             val changelog = project.rootProject.file("CHANGELOG.md")
@@ -227,14 +252,11 @@ tasks {
             }
 
             val libs = project.file("build/libs").listFiles().filter { it.name.endsWith(".jar") }
-            libs.forEach {
-                it.delete()
-            }
+            libs.forEach { it.delete() }
 
-            val devLibs = project.file("build/devlibs").listFiles().filter { it.name.endsWith(".jar") }
-            devLibs.forEach {
-                it.delete()
-            }
+            val devLibs =
+                    project.file("build/devlibs").listFiles().filter { it.name.endsWith(".jar") }
+            devLibs.forEach { it.delete() }
         }
     }
 }
@@ -259,13 +281,14 @@ configure<PublishingExtension> {
                 from(components["java"])
             }
         } else {
-            println("Not enabling Maven publishing")
+            println("Maven Publishing: ☒")
         }
     }
 
     repositories {
         if (enableMaven &&
-            (getGradleSecret("maven_username") != null && getGradleSecret("maven_password") != null)
+                        (getGradleSecret("maven_username") != null &&
+                                getGradleSecret("maven_password") != null)
         ) {
             maven {
                 name = "JamalamMavenRelease"
